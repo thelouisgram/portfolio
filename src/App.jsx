a7392956c9069125e3850666001122733ba8c083
thelouisgram <hadesanoye01@gmail.com>
2023-09-26 18:04:29 +0100

    animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from "react"
-import Contact from "./components/Contact"
-import Home from "./components/Home"
-import Navbar from "./components/Navbar"
-import Projects from "./components/Projects"
-import Skills from "./components/Skills"
-import MobileNavbar from "./components/MobileNavbar"
+import { useState } from "react";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import MobileNavbar from "./components/MobileNavbar";
+import RevealDiv from "./components/animation/RevealDiv";
 
 const App = () => {
-  const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(false);
   return (
     <div className="bg-[#23252C] w-full h-auto font-Space relative">
       <div className="w-full md:w-[1100px] md:mx-auto h-full ">
@@ -16,12 +17,18 @@ const App = () => {
       </div>
       <div className="w-full px-3 xs:px-5 md:px-0 md:w-[1100px] md:mx-auto h-auto ">
         <Home />
-        <Skills />
-        <Projects />
-        <Contact />
+        <RevealDiv>
+          <Skills />
+        </RevealDiv>
+        <RevealDiv>
+          <Projects />
+        </RevealDiv>
+        <RevealDiv>
+          <Contact />
+        </RevealDiv>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/animation/RevealDiv.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+const RevealDiv = ({ children }) => {
+    const [ref, inView] = useInView({
+        triggerOnce: true,
+        threshold: 0.1,
+    });
+
+    const variants = {
+        hidden: { opacity: 0, y: 100 },
+        visible: { opacity: 1, y: 0 },
+    };
+
+    return (
+        <motion.div
+            ref={ref}
+            initial="hidden"
+            animate={inView ? "visible" : "hidden"}
+            variants={variants}
+            transition={{ duration: 0.8 }}
+            className="reveal-div"
+        >
+            {children}
+        </motion.div>
+    );
+};
+
+export default RevealDiv;
